0803de61ac55a120966478c53eab76311f4ca3b7
Joseph <joseph@verifiedjoseph.com>
2021-10-25 18:11:34 +0100

    [Endpoint\Health] Create file

---
+++ src/Gotify/Endpoint/Health.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Gotify\Endpoint;
+
+Use Gotify\Api;
+
+/**
+ * Class for interacting with health API endpoint
+ */
+class Health extends Api
+{
+	/** @var string $endpoint API endpoint */
+	private string $endpoint = 'health';
+
+	/**
+	 * Get health information
+	 * 
+	 * @return \stdClass
+	 */
+	public function get()
+	{
+		return $this->guzzle->get($this->endpoint);
+	}
+} 
