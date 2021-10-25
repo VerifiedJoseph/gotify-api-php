e850fdd70f01bfce0659d933c2287f883636bcf5
Joseph <joseph@verifiedjoseph.com>
2021-10-26 00:09:39 +0100

    [Endpoint\Plugin] Add class

---
+++ src/Gotify/Endpoint/Plugin.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Gotify\Endpoint;
+
+Use Gotify\Api;
+
+/**
+ * Class for interacting with plugin API endpoint
+ */
+class Plugin extends Api
+{
+	/** @var string $endpoint API endpoint */
+	private string $endpoint = 'plugin';
+
+	/**
+	 * Get all plugins
+	 *
+	 * @return \stdClass
+	 */
+	public function getAll()
+	{
+		return $this->guzzle->get($this->endpoint);
+	}
+
+	/**
+	 * Get configuration for Configurer plugin.
+	 *
+	 * @param string $id Plugin Id
+	 *
+	 * @return \stdClass
+	 */
+	public function getConfig(int $id)
+	{
+		return $this->guzzle->get($this->endpoint . '/' . $id . '/config');
+	}
+
+	/**
+	 * Update configuration for Configurer plugin.
+	 *
+	 * @param string $id Plugin Id
+	 *
+	 * @return \stdClass
+	 */
+	public function updateConfig(int $id)
+	{
+		// Todo: Add action
+	}
+
+	/**
+	 * Get display info for a Displayer plugin
+	 *
+	 * @param string $id Plugin Id
+	 *
+	 * @return \stdClass
+	 */
+	public function getDisplayInfo(int $id)
+	{
+		return $this->guzzle->get($this->endpoint . '/' . $id . '/display');
+	}
+
+	/**
+	 * Enable a plugin.
+	 *
+	 * @param string $id Plugin Id
+	 *
+	 * @return null
+	 */
+	public function enable(int $id)
+	{
+		$data = array();
+
+		return $this->guzzle->post($this->endpoint . '/' . $id . '/enable', $data);
+	}
+
+	/**
+	 * Disable a plugin
+	 *
+	 * @param string $id Plugin Id
+	 *
+	 * @return null
+	 */
+	public function disable(int $id)
+	{
+		$data = array();
+
+		return $this->guzzle->post($this->endpoint . '/' . $id . '/disable', $data);
+	}
+}
