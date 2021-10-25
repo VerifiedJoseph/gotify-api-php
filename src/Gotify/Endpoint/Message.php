a7201ae6476b92a0db7a9d383956e8047150045a
Joseph <joseph@verifiedjoseph.com>
2021-10-25 21:44:54 +0100

    [Endpoint\Message] Add class

---
+++ src/Gotify/Endpoint/Message.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace Gotify\Endpoint;
+
+Use Gotify\Api;
+
+/**
+ * Class for interacting with Message API endpoint
+ */
+class Message extends Api
+{
+	/** @var string $endpoint API endpoint */
+	private string $endpoint = 'message';
+
+	/**
+	 * Get all message
+	 * 
+	 * @return \stdClass
+	 */
+	public function getAll()
+	{
+		return $this->guzzle->get($this->endpoint);
+	}
+
+	/**
+	 * Create a message
+	 * 
+	 * @param string $title	Message title
+	 * @param string $message Message body
+	 * @param int $priority Message priority
+	 * @param array $extras Message extras
+	 * 
+	 * @return \stdClass
+	 */
+	public function create(string $title, string $message, int $priority = 0, array $extras = array())
+	{
+		$data = array(
+			'title' => $title,
+			'message' => $message,
+			'priority' => $priority
+		);
+
+		if (empty($extras) === false) {
+			$data['extras'] = $extras;
+		}
+
+		return $this->guzzle->post($this->endpoint, $data);
+	}
+
+	/**
+	 * Delete a message
+	 * 
+	 * @param string $id Message Id
+	 * 
+	 * @return null
+	 */
+	public function delete(int $id)
+	{
+		return $this->guzzle->delete($this->endpoint . '/' . $id);
+	}
+
+	/**
+	 * Delete all messages
+	 * 
+	 * @return null
+	 */
+	public function deleteAll()
+	{
+		return $this->guzzle->delete($this->endpoint);
+	}
+} 
