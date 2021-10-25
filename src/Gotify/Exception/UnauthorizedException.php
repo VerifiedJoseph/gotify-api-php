4056ca7640446e5deccb3e4709efb9e149c52be8
Joseph <joseph@verifiedjoseph.com>
2021-10-25 18:12:15 +0100

    Create UnauthorizedException.php

---
+++ src/Gotify/Exception/UnauthorizedException.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Gotify\Exception;
+
+use Throwable;
+
+class UnauthorizedException extends \Exception {
+	public function __construct($message, $code = 0, Throwable $previous = null) {
+		parent::__construct($message, $code, $previous);
+	}
+}
