2162458b34635748db7acad71e2c4d17ed93c762
Joseph <joseph@verifiedjoseph.com>
2021-11-07 19:25:25 +0000

    [Json] Add missing PHPdocs @throws to methods

--- src/Gotify/Json.php
+++ src/Gotify/Json.php
@@ -15,6 +15,8 @@ final class Json
 	 *
 	 * @param array<mixed> $data
 	 * @return string
+	 * 
+	 * @throws GotifyException if array could not be encoded
 	 */
 	static function encode(array $data)
 	{
@@ -31,6 +33,8 @@ final class Json
 	 *
 	 * @param string $json
 	 * @return \stdClass|array<mixed>
+	 * 
+	 * @throws GotifyException if JSON could not be decoded
 	 */
 	public static function decode(string $json)
 	{
