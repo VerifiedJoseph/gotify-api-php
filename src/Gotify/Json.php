f5285b29d00ab304c2199976d4e69826f83155b8
Joseph <joseph@verifiedjoseph.com>
2021-11-14 22:44:23 +0000

    Fix Json::decode issue reported by phpstan level 8 (#31)
    Commits:
    * Fix json_decode issue reported by phpstan level 8
    * [Gotify\Json] Run phpcbf

--- src/Gotify/Json.php
+++ src/Gotify/Json.php
@@ -34,15 +34,21 @@ final class Json
 	 * Decode JSON
 	 *
 	 * @param string $json
-	 * @return \stdClass|array<mixed>
+	 * @return stdClass|array<mixed>
 	 *
 	 * @throws GotifyException if JSON could not be decoded
 	 */
 	public static function decode(string $json): stdClass|array
 	{
 		try {
-			return json_decode($json, flags: JSON_THROW_ON_ERROR);
+			$decoded = json_decode($json, flags: JSON_THROW_ON_ERROR);
 
+			if (is_array($decoded)) {
+				return (array) $decoded;
+
+			} else {
+				return (object) $decoded;
+			}
 		} catch (JsonException $err) {
 			throw new GotifyException('JSON Error: ' . $err->getMessage());
 		}
