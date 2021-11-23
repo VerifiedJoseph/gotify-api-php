a2326e39a17ef1e507ed68b582429211ed2880ac
Joseph <joseph@verifiedjoseph.com>
2021-11-23 23:16:35 +0000

    test

---
+++ .ci/was-loaded.php
@@ -0,0 +1,22 @@
+<?php
+
+use Gotify\Exception\GotifyException;
+use Gotify\Exception\EndpointException;
+
+try {
+	// Set server
+	$server = new Gotify\Server('http://127.0.0.1:8080');
+
+	// Set application token
+	$auth = new Gotify\Auth\User('admin', 'admin');
+
+	$plugin = new Gotify\Endpoint\Plugin(
+		$server->get(),
+		$auth->get()
+	);
+
+	var_dump($plugin->getAll());
+
+} catch (EndpointException | GotifyException $err) {
+	echo $err->getMessage();
+}
