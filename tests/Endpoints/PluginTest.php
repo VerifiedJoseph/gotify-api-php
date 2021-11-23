0065c0fa91e8694715c9c30c8be536a206aa26d8
Joseph <joseph@verifiedjoseph.com>
2021-11-23 20:55:48 +0000

    Create PluginTest.php

---
+++ tests/Endpoints/PluginTest.php
@@ -0,0 +1,73 @@
+<?php
+
+class PluginTest extends TestCase
+{
+	private static Gotify\Endpoint\Plugin $plugin;
+
+	private static int $pluginId = 1;
+
+	public static function setUpBeforeClass(): void
+	{
+		parent::setUpBeforeClass();
+
+		self::$plugin = new Gotify\Endpoint\Plugin(
+			self::$server->get(),
+			self::$auth->get()
+		);
+	}
+
+	/**
+	 * Test getting all plugins
+	 */
+	public function testGetAll(): void
+	{
+		$plugins = self::$plugin->getAll();
+
+		$this->assertIsObject($plugins);
+		$this->assertObjectHasAttribute('plugins', $plugins);
+
+		if (count($plugins->plugins) > 0) {
+			$this->assertIsObject($plugins->plugins[0]);
+
+			$plugin = $plugins->plugins[0];
+
+			$this->assertObjectHasAttribute('id', $plugin);
+			$this->assertObjectHasAttribute('name', $plugin);
+			$this->assertObjectHasAttribute('token', $plugin);
+		}
+	}
+
+	/**
+	 * Test getting config for a plugin
+	 */
+	public function testGetConfig(): void
+	{
+		$config = self::$plugin->getConfig(self::$pluginId);
+
+		$this->assertNotEmpty($config);
+	}
+
+	/**
+	 * Test enabling a plugin
+	 */
+	public function testEnable(): void
+	{
+		$enabled = self::$plugin->enable(self::$pluginId);
+
+		$this->assertIsBool($enabled);
+		$this->assertEquals(true, $enabled);
+	}
+
+	/**
+	 * Test disabling a plugin
+	 *
+	 * @depends testEnable
+	 */
+	public function testDisable(): void
+	{
+		$disabled = self::$plugin->disable(self::$pluginId);
+
+		$this->assertIsBool($disabled);
+		$this->assertEquals(true, $disabled);
+	}
+}
