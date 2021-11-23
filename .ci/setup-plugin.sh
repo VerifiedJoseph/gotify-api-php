7f70c4e306319bcd694b753669e903af2c7c1841
Joseph <joseph@verifiedjoseph.com>
2021-11-23 22:19:08 +0000

    Update setup-plugin.sh

--- .ci/setup-plugin.sh
+++ .ci/setup-plugin.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Create gotify-data/plugins when download plugin gotify-broadcast
+# Create gotify-data/plugins then download plugin gotify-broadcast
 #
-FOLDER=gotify-data/pulgins
+FOLDER=gotify-data/plugins
 URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.1/broadcasts-linux-amd64-for-gotify-v2.0.23.so"
 
 mkdir -p $FOLDER
