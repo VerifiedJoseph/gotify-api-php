8d4669b21111e06ae5a9b2d7b180d37040f5b6ce
Joseph <joseph@verifiedjoseph.com>
2021-11-23 22:23:33 +0000

    [test.yml] Add support for testing plugin endpoints (#41)
    Updates GitHub action 'test.yml' to add support for testing the plugin endpoint using a downloaded plugin.
    
    Commits:
    * [ci\docker-compose.yml] Add volume
    * [test.yml] Add step for fetching the Gotify Broadcaster pulgin
    * [test.yml] Run tests if test.yml is updated
    * [.ci\setup-pulgin.sh] Add script
    * [.ci\docker-compose.yml] Fix missing httpbin
    * [.ci\docker-compose.yml] Fix formatting
    * Fix spelling
    * Update setup-plugin.sh

---
+++ .ci/setup-plugin.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Create gotify-data/plugins then download plugin gotify-broadcast
+#
+FOLDER=gotify-data/plugins
+URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.1/broadcasts-linux-amd64-for-gotify-v2.0.23.so"
+
+mkdir -p $FOLDER
+cd $FOLDER
+
+wget $URL
