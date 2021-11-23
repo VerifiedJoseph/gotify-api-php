86878e93b92a93adc6a2e848faca701a31da6278
Joseph <joseph@verifiedjoseph.com>
2021-11-23 22:35:18 +0000

    Update setup-plugin.sh

--- .ci/setup-plugin.sh
+++ .ci/setup-plugin.sh
@@ -5,6 +5,5 @@ FOLDER=gotify-data/plugins
 URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.1/broadcasts-linux-amd64-for-gotify-v2.0.23.so"
 
 mkdir -p $FOLDER
-cd $FOLDER
 
-wget $URL
+wget $URL -P $FOLDER
