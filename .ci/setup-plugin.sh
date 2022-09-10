09f0d43f192ce5f528e3ebc38ca42205fccd5928
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-09-10 21:50:27 +0100

    [.ci\setup-plugin.sh] Use Unix line endings

--- .ci/setup-plugin.sh
+++ .ci/setup-plugin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Create folder gotify-data/plugins then download plugin gotify-broadcast
+# Create folder gotify-data/plugins then download plugin gotify-broadcast.
 #
 FOLDER=~/gotify-data/plugins
 URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.1/broadcasts-linux-amd64-for-gotify-v2.0.23.so"
