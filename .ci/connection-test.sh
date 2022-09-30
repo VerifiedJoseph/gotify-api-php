93ef98fa0d9923369a5ef0b7907fd8d8ab050a02
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-09-30 19:52:24 +0100

    Update connection-test.sh

--- .ci/connection-test.sh
+++ .ci/connection-test.sh
@@ -3,13 +3,11 @@
 # 
 # Test connections to docker containers
 
-sleep 5
-
 connect()
 {
 	echo "Testing connection to ${1}"
 
-	curl --retry 5 --retry-connrefused --retry-delay 10 ${1}
+	curl -s -o /dev/null ${1}
 	if [ $? -ne 0 ]
 		then
 			echo "Failed. Aborting job";
@@ -19,5 +17,9 @@ connect()
 	echo "OK"
 }
 
+# Waiting
+sleep 5
+
+# Test
 connect ${GOTIFY_URI}
 connect ${HTTPBIN_URI}
