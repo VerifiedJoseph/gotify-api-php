ac1e939c834735635ad06c373ddb794f692a1148
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-09-30 19:41:00 +0100

    Update connection-test.sh

--- .ci/connection-test.sh
+++ .ci/connection-test.sh
@@ -7,13 +7,11 @@ connect()
 {
 	echo "Testing connection to ${1}"
 
-	curl --retry 5 --retry-connrefused --retry-delay 2 -s -o /dev/null ${1}
+	curl --retry 5 --retry-connrefused --retry-delay 10 ${1}
 	if [ $? -ne 0 ]
 		then
 			echo "Failed. Aborting job";
 			exit 1
-
-			echo "s";
 	fi
 
 	echo "OK"
