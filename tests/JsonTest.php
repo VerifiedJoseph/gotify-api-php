68b33edcdc0711129e5be303c8a7c42394302019
Joseph <VerifiedJoseph@users.noreply.github.com>
2022-10-03 14:25:35 +0000

    Add tests for Json class (#140)
    * Add tests for Json class
    * Add stdClass

---
+++ tests/JsonTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use stdClass;
+use Gotify\Json;
+use Gotify\Exception\GotifyException;
+
+class JsonTest extends TestCase
+{
+	public function testEncodeValid(): void
+	{
+		self::assertEquals('{"foo":"bar"}', Json::encode(['foo' => 'bar']));
+	}
+
+	public function testDecodeValid(): void
+	{
+		$expected = new stdClass();
+		$expected->foo = 'bar';
+		self::assertEquals($expected, Json::decode('{"foo": "bar"}'));
+	}
+
+	public function testDecodeInvalid(): void
+	{
+		$this->expectException(GotifyException::class);
+		$this->expectExceptionMessage('JSON Error: Syntax error');
+		Json::decode('foo');
+	}
+}
