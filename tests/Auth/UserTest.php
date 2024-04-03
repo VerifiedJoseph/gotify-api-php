64aa8a7820580aa17a4315962f82477fb4b28df8
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-03 12:52:11 +0100

    Create UserTest.php

---
+++ tests/Auth/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Auth;
+
+use PHPUnit\Framework\Attributes\CoversClass;
+use Gotify\Auth\User;
+use AbstractTestCase;
+
+#[CoversClass(User::class)]
+#[CoversClass(\Gotify\Auth::class)]
+class UserTest extends AbstractTestCase
+{
+    private string $method = 'user';
+    private string $username = 'username';
+    private string $password = 'password';
+
+    /**
+     * Test `getAuthMethod()`
+     */
+    public function testGetAuthMethod(): void
+    {
+        $auth = new User($this->username, $this->password);
+        $this->assertEquals($this->method, $auth->getAuthMethod());
+    }
+
+    /**
+     * Test `getUsername()`
+     */
+    public function testGetUsername(): void
+    {
+        $auth = new User($this->username, $this->password);
+        $this->assertEquals($this->username, $auth->getUsername());
+    }
+
+    /**
+     * Test `getPassword()`
+     */
+    public function testGetPassword(): void
+    {
+        $auth = new User($this->username, $this->password);
+        $this->assertEquals($this->password, $auth->getPassword());
+    }
+}
