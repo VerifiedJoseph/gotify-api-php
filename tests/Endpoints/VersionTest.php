463218667a4de1f301eac17a39a05a5b79d82777
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-04-03 12:00:38 +0100

    Update VersionTest.php

--- tests/Endpoints/VersionTest.php
+++ tests/Endpoints/VersionTest.php
@@ -1,7 +1,14 @@
 <?php
 
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\UsesClass;
 use Gotify\Endpoint\Version;
 
+#[CoversClass(Version::class)]
+#[UsesClass(Gotify\Api::class)]
+#[UsesClass(Gotify\Guzzle::class)]
+#[UsesClass(Gotify\Json::class)]
+#[UsesClass(Gotify\Server::class)]
 class VersionTest extends AbstractTestCase
 {
     /**
