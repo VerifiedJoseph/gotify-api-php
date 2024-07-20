8ca460b91f3283f6b202cd2080d6a1a0bb775b60
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-07-20 19:54:43 +0100

    Update ClientTest.php

--- tests/Endpoints/ClientTest.php
+++ tests/Endpoints/ClientTest.php
@@ -14,9 +14,7 @@ use Gotify\Endpoint\AbstractEndpoint;
 #[UsesClass(\Gotify\Guzzle::class)]
 #[UsesClass(\Gotify\Json::class)]
 #[UsesClass(\Gotify\Server::class)]
-#[UsesClass(\Gotify\Auth\AbstractAuth::class)]
 #[UsesClass(\Gotify\Auth\User::class)]
-#[UsesClass(\Gotify\Server::class)]
 #[UsesClass(\Gotify\Auth\AbstractAuth::class)]
 class ClientTest extends AbstractTestCase
 {
