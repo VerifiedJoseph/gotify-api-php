99e480df3b1f99f6eaa3678fa5bdee25c21c443a
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-11-01 17:18:37 +0000

    Add testsuite for plugin endpoint (#341)

--- .ci/scripts/setup-plugin.sh
+++ .ci/scripts/setup-plugin.sh
@@ -2,7 +2,7 @@
 # Create folder gotify-data/plugins then download plugin gotify-broadcast.
 #
 FOLDER=~/gotify-data/plugins
-URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.1/broadcasts-linux-amd64-for-gotify-v2.0.23.so"
+URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.4/broadcasts-linux-amd64-for-gotify-v2.5.0.so"
 
 mkdir -p $FOLDER
 
