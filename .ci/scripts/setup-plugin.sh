97ec654667a032c873c3461e1255ba865c42a23e
Joseph <VerifiedJoseph@users.noreply.github.com>
2024-12-10 11:21:17 +0000

    Bump gotify/server from 2.5.0 to 2.6.1 (#431)

--- .ci/scripts/setup-plugin.sh
+++ .ci/scripts/setup-plugin.sh
@@ -2,7 +2,7 @@
 # Create folder gotify-data/plugins then download plugin gotify-broadcast.
 #
 FOLDER=~/gotify-data/plugins
-URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.4/broadcasts-linux-amd64-for-gotify-v2.5.0.so"
+URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.5/broadcasts-linux-amd64-for-gotify-v2.6.0.so"
 
 mkdir -p $FOLDER
 
