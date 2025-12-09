6746060301a9b4fee80871b7fd0038f9a825543c
Joseph <VerifiedJoseph@users.noreply.github.com>
2025-12-09 17:53:49 +0000

    ci: Update gotify broadcast plugin from 0.3.5 to 0.3.6 (#514)

--- .ci/scripts/setup-plugin.sh
+++ .ci/scripts/setup-plugin.sh
@@ -2,7 +2,7 @@
 # Create folder gotify-data/plugins then download plugin gotify-broadcast.
 #
 FOLDER=~/gotify-data/plugins
-URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.5/broadcasts-linux-amd64-for-gotify-v2.6.0.so"
+URL="https://github.com/eternal-flame-AD/gotify-broadcast/releases/download/v0.3.6/broadcasts-linux-amd64-for-gotify-v2.7.3.so"
 
 mkdir -p $FOLDER
 
